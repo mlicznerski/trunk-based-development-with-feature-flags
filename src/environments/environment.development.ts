5058bcf1ea96c2eedccbf832567eddb618d6f576
Marcin Licznerski <licznerski.marcin@gmail.com>
2023-07-13 16:34:02 +0200

    Feature3 - Refactor is finished
     - Enable Feature for both prod and dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,6 @@ export const environment: Environment = {
   production: false,
   features: {
     'dev-ticket-2-feature2': true,
+    'dev-ticket-3-feature3-refactor--show-refactored-component': true,
   },
 };
