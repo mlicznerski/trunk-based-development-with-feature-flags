00d30bf3ae7809fd008b719c0c59788c384030e7
Marcin Licznerski <licznerski.marcin@gmail.com>
2023-07-13 12:43:48 +0200

    Move components to separate directory for readability

--- src/app/modules/feature-1/components/feature3-to-be-refactored.component.ts
+++ src/app/modules/feature-1/components/feature3-to-be-refactored.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feature3-to-be-refactored',
-  template: `To be refactored`,
+  template: `Feature3 - To be refactored`,
   styles: [``],
 })
 export class Feature3ToBeRefactoredComponent implements OnInit {
--- src/app/modules/feature-1/feature1.module.ts
+++ src/app/modules/feature-1/feature1.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Feature1HomeComponent } from './containers/feature1-home.component';
-import { Feature2InitiallyHiddenComponent } from './containers/feature2-initially-hidden.component';
+import { Feature2InitiallyHiddenComponent } from './components/feature2-initially-hidden.component';
 import { FeatureFlagsModule } from '../feature-flags';
-import { Feature3ToBeRefactoredComponent } from './containers/feature3-to-be-refactored.component';
+import { Feature3ToBeRefactoredComponent } from './components/feature3-to-be-refactored.component';
 
 const routes: Routes = [{ path: '', component: Feature1HomeComponent }];
 
